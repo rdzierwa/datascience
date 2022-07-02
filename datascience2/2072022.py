9cbb4f9f404c134c7aaefb6a5144644694f4e202
rdzierwa <61204789+rdzierwa@users.noreply.github.com>
2022-07-02 15:29:54 +0200

    yy

--- datascience2/2072022.py
+++ datascience2/2072022.py
@@ -1,4 +1,5 @@
-
+from asyncio import constants
+from enum import Enum
 import math
 from math import pi
 
@@ -13,19 +14,22 @@ class Figura:
     def pole(self):  # S/P
         """Obliczanie pola powierzchni."""
         raise NotImplementedError
-
 class Kolo(Figura):
 
-    def __init__(self,promien):
+    color = "Niebieski"
+    
+    def __init__(self, promien):
         super().__init__()
         self.promien = promien
 
     def obwod(self):
-        return f'Obwód to: {2* pi * self.promien}'
+        return f'Obwód to: {2 * pi * self.promien}'
 
     def pole(self):
-        return f'Pole koła to {pi* self.promien**2}'
-        
+        return f'Pole koła to {pi * self.promien ** 2}'
+
+    def show(self, color = color):
+        print (f'prostokąt jest {color}')
 
 
 class Kwadrat(Kolo):
@@ -39,7 +43,7 @@ class Kwadrat(Kolo):
         return f'{super().pole()}'
 
     def pole(self):
-        return f'{self.a*self.b}'
+        return f'{self.a * self.b}'
 
 # kolo = Kolo(5)
 # print(kolo.obwod())
@@ -49,3 +53,86 @@ class Kwadrat(Kolo):
 
 # print(kw.pole_kola())
 # print(kw.pole())
+
+# kolo = Kolo(5)
+# kolo.color = 'ROŻ'
+# print(kolo.color)
+# kolo.show()
+
+class Test():
+    COLOR = 'NIEBIESKI'
+    def __init__(self) -> None:
+        pass
+        
+    def show(self, color = COLOR):
+        return (f'Mamy kolor {color} {id(Test)}')
+    def show2(self):
+        return (f'Bez coloru {id(Test)}')
+
+# t1 = Test()
+# t1.COLOR = 'ZIELONY'
+# print (t1.COLOR)
+# print (t1.show())
+# print (t1.COLOR)
+# print (t1.show2())
+import sqlite3
+import os
+
+BASE = f'{os.getcwd()}/pythonprojects/datascience/datascience2/'
+url_sql = f'{BASE}library_db.db'
+url_sql2 = f'{BASE}CodeBrainers.db'
+#print(url_sql)
+url_sql2 = f'datascience2/CodeBrainers.db'
+
+def run_execute(sql):
+    try:
+        return cur.execute(sql).fetchall()
+    except Exception as Error:
+        return Error
+con = sqlite3.connect(url_sql2)
+cur = con.cursor()
+
+sql2 = "SELECT * FROM authors"
+sql1 = "SELECT sqlite_version()"
+sql3 = "SELECT * FROM Product"
+sql_show_table = "SELECT name FROM sqlite_schema WHERE type ='table' AND name NOT LIKE 'sqlite_%';"
+create_tabel = """CREATE TABLE Users(
+                    login VARCHAR(8) NOT NULL, 
+                    name VARCHAR(40) NOT NULL, 
+                    phone_no VARCHAR(15)
+                )"""
+drop_city_table = "DROP TABLE IF EXISTS  City;"
+create_table_city = """ CREATE TABLE City(
+                            id SMALLINT,
+                            name VARCHAR(30) NOT NULL, 
+                            city VARCHAR(35) NOT NULL
+                        )
+                    """
+sql_count = " SELECT COUNT(id) FROM City"
+sql_insert_data_city = " INSERT INTO City VALUES (?,?,?)"
+sql_insert_one_data_city = "INSERT INTO City VALUES (((SELECT MAX(id) FROM CITY)+1),?,?)"
+data = run_execute(create_table_city)
+con.commit()
+print(run_execute(sql_count)[0][0])
+#data_to_city = [
+ #       (5001, 'Piotr Nowak','Warszawa'),
+ #       (5002, 'Anna Kowalska','Kraków'),
+  #      (5003, 'Krzysztof Wiśniewski','Łódź'),
+  #      (5004, 'Maria Wójcik','Kraków'),
+  #      (5005, 'Andrzej Kowalczyk','Wrocław')
+#]
+#cur.executemany(sql_insert_data_city,data_to_city)
+#con.commit()
+name = str(input('Podaj imię i nazwisko:'))
+city = str(input('Podaj miasto:'))
+cur.execute(sql_insert_one_data_city,(name,city))
+con.commit()
+print(run_execute(sql_count)[0][0])
+
+print(data)
+
+con.close()
+
+
+
+
